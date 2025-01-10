f45ba423518deb663b419c5437b59d6432da8616
villarley <115122095+Villarley@users.noreply.github.com>
2025-01-09 20:43:50 -0600

    feat: updated metadata

--- packages/nextjs/components/ScaffoldStarkAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldStarkAppWithProviders.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
 import { StarknetConfig, starkscan } from "@starknet-react/core";
 import { Header } from "~~/components/Header";
@@ -13,25 +12,12 @@ import { useNativeCurrencyPrice } from "~~/hooks/scaffold-stark/useNativeCurrenc
 
 const ScaffoldStarkApp = ({ children }: { children: React.ReactNode }) => {
   useNativeCurrencyPrice();
-  const { resolvedTheme } = useTheme();
-  const isDarkMode = resolvedTheme === "dark";
   return (
     <>
       <div className="flex relative flex-col min-h-screen bg-main">
-        {isDarkMode ? (
-          <>
-            <div className="circle-gradient-dark w-[330px] h-[330px]"></div>
-            <div className="circle-gradient-blue-dark w-[330px] h-[330px]"></div>
-          </>
-        ) : (
-          <>
-            <div className="circle-gradient w-[330px] h-[330px]"></div>
-            <div className="circle-gradient-blue w-[330px] h-[630px]"></div>
-          </>
-        )}
         <Header />
         <main className="relative flex flex-col flex-1">{children}</main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
       <Toaster />
     </>
